a20dd2117f687fea6ba08f1c37f7f0518e442898
KendiAstro23 <litalakendy975@gmail.com>
2025-02-13 12:00:48 +0300

    login to landing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
-def create
-  user = User.find_by(email: params[:email])
-  if user&.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to books_path, notice: "Logged in successfully!" # Redirect to dashboard
-  else
-    flash[:alert] = "Invalid email or password"
-    redirect_to root_path # Return to unified landing page
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to books_path, notice: "Logged in successfully!" # Redirect to dashboard
+    else
+      flash[:alert] = "Invalid email or password"
+      redirect_to root_path # Return to unified landing page
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out successfully!"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
-def create
-  @user = User.new(user_params)
-  if @user.save
-    session[:user_id] = @user.id
-    redirect_to books_path, notice: "Account created successfully!" # Redirect to dashboard
-  else
-    flash[:alert] = @user.errors.full_messages.join(", ")
-    redirect_to root_path # Return to unified landing page
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to books_path, notice: "Account created successfully!" # Redirect to dashboard
+    else
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      render :new, status: :unprocessable_entity # Stay on the signup page with error messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
